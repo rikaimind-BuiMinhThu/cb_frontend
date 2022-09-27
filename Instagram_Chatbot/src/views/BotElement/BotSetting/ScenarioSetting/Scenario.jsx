2918402ea418468427606462fb7f36cabc052cca
huan.tran2 <huan.tran@rikai.technology>
2022-09-27 13:52:13 +0700

    scenario single UI init

--- Instagram_Chatbot/src/views/BotElement/BotSetting/ScenarioSetting/Scenario.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/ScenarioSetting/Scenario.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import '../../../../assets/css/bot/scenario/scenario-single.css';
+import { useEffect } from 'react';
+import { Col, Row, Card, CardBody, CardHeader, Button } from 'reactstrap';
 
-function Scenario() {
+const Scenario = () => {
+  // states
+
+  // side effects
+  useEffect(() => {
+    document.title = 'Edit Scenario';
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div>Scenario</div>
-  )
-}
+    <div className="content">
+      <div className="ss-actions">
+        <Button>Save scenario</Button>
+        <Button>Save and preview</Button>
+      </div>
+      <Row>
+        <Col md="6">
+          <Card>
+            <CardBody></CardBody>
+          </Card>
+        </Col>
+        <Col md="6">
+          <Card>
+            <CardBody></CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default Scenario
+export default Scenario;
