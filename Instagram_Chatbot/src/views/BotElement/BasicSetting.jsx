cac3d4622ae898b62cc51d25c985c25f3e6eb0ff
ntyen.nguyen <ntyen.nguyen@rikai.technology>
2022-10-05 15:37:48 +0700

    Edit basic setting css

--- Instagram_Chatbot/src/views/BotElement/BasicSetting.jsx
+++ Instagram_Chatbot/src/views/BotElement/BasicSetting.jsx
@@ -85,18 +85,18 @@ function BasicSetting() {
               </CardHeader>
               <CardBody>
                 <form id='form-basic-setting'>
-                  <div className='field-container'>
-                    <span className='field-lable'>Full name</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>Full name</span>
+                    <div className='bs-field-input'>
                       <input id='fullname' type='text' placeholder='enter input ...' name='full_name' defaultValue={userDetail.full_name}
                         onChange={() => utils.checkInput('fullname', 'errFullname', 'Full name')}></input>
-                      <span id="errFullname" className='err-format'></span>
+                      <span id="errFullname" className='bs-err-format'></span>
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>Business category</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>Business category</span>
+                    <div className='bs-field-input'>
                       <select name='business_division' value={userDetail.business_division}>
                         <option value='sole_proprietorship'>Sole proprietorship</option>
                         <option value='corporation'>Corporation</option>
@@ -104,72 +104,72 @@ function BasicSetting() {
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>Company name</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>Company name</span>
+                    <div className='bs-field-input'>
                       <input id='companyName' type='text' placeholder='enter input ...' name='company_name' defaultValue={userDetail.company_name}
                         onChange={() => utils.checkMaxLength('companyName', 'errCompanyname', 'Company name', 50)}></input>
-                      <span id="errCompanyname" className='err-format'></span>
+                      <span id="errCompanyname" className='bs-err-format'></span>
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>Department</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>Department</span>
+                    <div className='bs-field-input'>
                       <input id='department' type='text' placeholder='enter input ...' name='department' defaultValue={userDetail.department}
                         onChange={() => utils.checkMaxLength('department', 'errDepartment', 'Department', 50)}></input>
-                      <span id="errDepartment" className='err-format'></span>
+                      <span id="errDepartment" className='bs-err-format'></span>
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>Position</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>Position</span>
+                    <div className='bs-field-input'>
                       <input id='job_title' type='text' placeholder='enter input ...' name='job_title' defaultValue={userDetail.job_title}
                         onChange={() => utils.checkMaxLength('job_title', 'errPosition', 'Position', 50)}></input>
-                      <span id="errPosition" className='err-format'></span>
+                      <span id="errPosition" className='bs-err-format'></span>
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>Email address</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>Email address</span>
+                    <div className='bs-field-input'>
                       <input id='emailAddress' type='text' placeholder='enter input ...' name='email' defaultValue={userDetail.email}
                         onChange={() => utils.checkAddress('emailAddress', 'errEmailAddress', 'Email address')}></input>
-                      <span id="errEmailAddress" className='err-format'></span>
+                      <span id="errEmailAddress" className='bs-err-format'></span>
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>phone number</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>phone number</span>
+                    <div className='bs-field-input'>
                       <input id='phone_number' type='number' placeholder='enter input ...' name='phone_number' defaultValue={userDetail.phone_number}
                         onChange={() => utils.checkMaxLength('phone_number', 'errPhone', 'phone number', 12)}></input>
-                      <span id="errPhone" className='err-format'></span>
+                      <span id="errPhone" className='bs-err-format'></span>
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>post code</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>post code</span>
+                    <div className='bs-field-input'>
                       <input id='post_code' type='number' placeholder='enter input ...' name='post_code' defaultValue={userDetail.post_code}
                         onChange={() => utils.checkMaxLength('post_code', 'errPostCost', 'post code', 50)}></input>
-                      <span id="errPostCost" className='err-format'></span>
+                      <span id="errPostCost" className='bs-err-format'></span>
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>address</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>address</span>
+                    <div className='bs-field-input'>
                       <input id='address' type='text' placeholder='enter input ...' name='address' defaultValue={userDetail.address}
                         onChange={() => utils.checkMaxLength('address', 'errAddress', 'address', 50)}></input>
-                      <span id="errAddress" className='err-format'></span>
+                      <span id="errAddress" className='bs-err-format'></span>
                     </div>
                   </div>
 
-                  <div className='field-container'>
-                    <span className='field-lable'>language</span>
-                    <div className='field-input'>
+                  <div className='bs-field-container'>
+                    <span className='bs-field-lable'>language</span>
+                    <div className='bs-field-input'>
                       <select name='language' value={userDetail.language}>
                         <option value='japanese'>Japanese</option>
                         <option value='english'>English</option>
@@ -180,7 +180,7 @@ function BasicSetting() {
                   </div>
                 </form>
 
-                <div className='field-btn'>
+                <div className='bs-field-btn'>
                   <button className='btn btn-primary' onClick={() => onSave()}>Keep</button>
                 </div>
               </CardBody>
