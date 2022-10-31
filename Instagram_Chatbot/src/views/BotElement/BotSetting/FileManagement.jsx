8c5c3b03c636afdbee72aa7df4f09e37d135e4c2
ntyen.nguyen <ntyen.nguyen@rikai.technology>
2022-10-31 17:13:43 +0700

    api file mng

--- Instagram_Chatbot/src/views/BotElement/BotSetting/FileManagement.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/FileManagement.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card, CardHeader, CardBody, CardTitle, Table, Row, Col } from 'reactstrap';
 import { Button } from 'react-bootstrap';
 import { useRef } from 'react';
@@ -8,32 +8,42 @@ import noImage from './../../../assets/img/no-image.jpg';
 import './../../../assets/css/file-mng.css';
 import api from '../../../api/api-management';
 import axios from 'axios';
+import ModalNoti from 'views/Popup/ModalNoti';
 
 function FileManagement() {
-  // const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([]);
   const [newFile, setNewFile] = useState(null);
   const [src, setSrc] = React.useState('');
   const [isOpenPreview, setIsOpenPreview] = useState(false);
   const inputRef = useRef(null);
   const [fileError, setFileError] = useState('');
+  const [isOpenNoti, setIsOpenNoti] = useState(false);
+  const [msgNoti, setMsgNoti] = useState();
+  const [isOpenDelete, setIsOpenDelete] = useState(false);
+  const [idFile, setIdFile] = useState();
 
   function handleUpload() {
     inputRef.current.click();
     setFileError('');
   }
 
-  function handleChangeFile(e) {
-    // if (files == []) setFiles([e.target.files[0]]);
-    // else {
-    //     setFiles([...files, e.target.files[0]]);
-    // }
+  function reload() {
+    api.get(`/api/v1//managements/file`).then((res) => {
+      console.log('files', res.data.data);
+      setFiles(res.data.data);
+    });
+  }
 
+  useEffect(() => {
+    reload();
+  }, []);
+
+  function handleChangeFile(e) {
     setNewFile(e.target.files[0]);
   }
 
   function handleSave() {
     console.log(newFile);
-    var urlFile = 'aa';
     const type = newFile.name.split('.')[1];
     //jpeg, jpg, png
 
@@ -43,14 +53,43 @@ function FileManagement() {
       api
         .post(`/api/v1/managements/file/upload`, file)
         .then((res) => {
-          urlFile = res.data.data.url;
+          const urlFile = res.data.data.url;
+          let filePost = { user_file: { file_type: type, file_url: res.data.data.path } };
           axios
-            .put(urlFile, newFile)
+            .put(urlFile, newFile, {
+              headers: {
+                'Content-Type': `image/${type}`,
+              },
+            })
             .then((res) => {
-              console.log('respon', res);
+              console.log('response`: ', res);
             })
             .catch((err) => {
-              console.log('err', err);
+              console.log('err: ', err);
+            });
+          api
+            .post(`/api/v1/managements/file`, filePost)
+            .then((res) => {
+              if (res.data.code == 1) {
+                console.log(res);
+                reload();
+                setMsgNoti(`Add successfully!`);
+                setIsOpenNoti(true);
+                setTimeout(() => {
+                  setIsOpenNoti(false);
+                  setMsgNoti(``);
+                }, 2000);
+              } else {
+                setMsgNoti(`Add failed!`);
+                setIsOpenNoti(true);
+                setTimeout(() => {
+                  setIsOpenNoti(false);
+                  setMsgNoti(``);
+                }, 2000);
+              }
+            })
+            .catch((err) => {
+              console.log(err);
             });
         })
         .catch((err) => {
@@ -61,20 +100,56 @@ function FileManagement() {
     }
   }
 
-  function handlePreview(file) {
-    console.log(file);
-    console.log(URL.createObjectURL(file));
-
-    const imageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-    if (imageTypes.includes(file.type)) {
-      setSrc(URL.createObjectURL(file));
-    } else {
-      setSrc(noImage);
-    }
+  function handlePreview(file_url) {
+    console.log(file_url);
+    // console.log(URL.createObjectURL(file_url));
+    // const imageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+    setSrc(`https://ec-chatbot.s3.ap-northeast-1.amazonaws.com/${file_url}`);
     setIsOpenPreview(true);
   }
 
-  function handleDelete(file) {}
+  function openDelete(id) {
+    setIsOpenDelete(true);
+    setIdFile(id);
+  }
+
+  function handleDelete() {
+    api
+      .delete(`/api/v1/managements/file/${idFile}`)
+      .then((res) => {
+        if (res.data.code == 1) {
+          console.log(res);
+          setIsOpenDelete(false);
+          setMsgNoti(`Delete successfully!`);
+          setIsOpenNoti(true);
+          reload();
+          setTimeout(() => {
+            setIsOpenNoti(false);
+            setMsgNoti(``);
+          }, 2000);
+        } else {
+          setMsgNoti(`Delete failed!`);
+          setIsOpenNoti(true);
+          setTimeout(() => {
+            setIsOpenNoti(false);
+            setMsgNoti(``);
+          }, 2000);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function handleCopy(file_url) {
+    navigator.clipboard.writeText(file_url);
+    setMsgNoti(`Copy successfully!`);
+    setIsOpenNoti(true);
+    setTimeout(() => {
+      setIsOpenNoti(false);
+      setMsgNoti(``);
+    }, 2000);
+  }
 
   return (
     <>
@@ -132,30 +207,50 @@ function FileManagement() {
                   <thead className="text-primary">
                     <tr>
                       <th style={{ width: '10%' }}>ID</th>
-                      <th style={{ width: '20%' }}>File name</th>
+                      {/* <th style={{ width: '20%' }}>File name</th> */}
                       <th style={{ width: '15%' }}>Type</th>
-                      <th style={{ width: '20%' }}>Size</th>
+                      {/* <th style={{ width: '20%' }}>Size</th> */}
                       <th style={{ width: '20%' }}>Url</th>
                       <th style={{ width: '250px', minWidth: '250px' }}>Action</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {/* {files.map((file, i) => (
-                                            <tr key={i}>
-                                                <td className="file-mng__border-table">{i}</td>
-                                                <td className="file-mng__border-table">{file.name}</td>
-                                                <td className="file-mng__border-table">{file.type}</td>
-                                                <td className="file-mng__border-table">{file.size}</td>
-                                                <td className="file-mng__border-table">{URL.createObjectURL(file)}</td>
-                                                <td className="file-mng__border-table file-mng__action-table">
-                                                    <div className="file-mng__action-wrapper">
-                                                        <button className="file-mng__btn-edit" onClick={() => handlePreview(file)}>Preview</button>
-                                                        <button className="file-mng__btn-stop">Copy</button>
-                                                        <button className="file-mng__btn-delete" onClick={() => handleDelete(file)}>Delete</button>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        ))} */}
+                    {files.map((file, i) => (
+                      <tr key={i}>
+                        <td className="file-mng__border-table">{file.id}</td>
+                        {/* <td className="file-mng__border-table">{file.name}</td> */}
+                        <td className="file-mng__border-table">{file.file_type}</td>
+                        {/* <td className="file-mng__border-table">{file.size}</td> */}
+                        {/* <td className="file-mng__border-table">{URL.createObjectURL(file)}</td> */}
+                        <td className="file-mng__border-table">{`https://ec-chatbot.s3.ap-northeast-1.amazonaws.com/${file.file_url}`}</td>
+                        <td className="file-mng__border-table file-mng__action-table">
+                          <div className="file-mng__action-wrapper">
+                            <button
+                              className="file-mng__btn-edit"
+                              onClick={() => handlePreview(file.file_url)}
+                            >
+                              Preview
+                            </button>
+                            <button
+                              className="file-mng__btn-stop"
+                              onClick={() => {
+                                handleCopy(
+                                  `https://ec-chatbot.s3.ap-northeast-1.amazonaws.com/${file.file_url}`
+                                );
+                              }}
+                            >
+                              Copy
+                            </button>
+                            <button
+                              className="file-mng__btn-delete"
+                              onClick={() => openDelete(file.id)}
+                            >
+                              Delete
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                   <tbody></tbody>
                 </Table>
@@ -165,10 +260,25 @@ function FileManagement() {
         </Row>
 
         <ModalShort open={isOpenPreview} onClose={() => setIsOpenPreview(false)}>
-          <div style={{ width: '300px', textAlign: 'center', color: '#51cbce' }}>
-            <img src={src} />
+          <div className="file-mng__preview_img">
+            <img src={src} alt="" />
+            <Button onClick={() => setIsOpenPreview(false)}>Close</Button>
           </div>
         </ModalShort>
+
+        <ModalShort open={isOpenDelete} onClose={() => setIsOpenDelete(false)}>
+          <div style={{ width: '300px', textAlign: 'center', color: '#51cbce' }}>
+            <h4>Do you want to delete file?</h4>
+            <Button onClick={() => handleDelete()}>Yes</Button>
+            <Button onClick={() => setIsOpenDelete(false)}>No</Button>
+          </div>
+        </ModalShort>
+
+        <ModalNoti open={isOpenNoti} onClose={() => setIsOpenNoti(false)}>
+          <div style={{ width: '300px', textAlign: 'center', color: '#51cbce' }}>
+            <span style={{ fontSize: '16px' }}>{msgNoti}</span>
+          </div>
+        </ModalNoti>
       </div>
     </>
   );
