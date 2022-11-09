f093d3b95950f387a20c4790214daf7db0d377bb
ntyen.nguyen <ntyen.nguyen@rikai.technology>
2022-11-09 15:25:17 +0700

    validate datepicker

--- Instagram_Chatbot/src/views/BotElement/BotSetting/Report/Report.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/Report/Report.jsx
@@ -5,20 +5,45 @@ import './../../../../assets/css/bot/report.css';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { format } from 'date-fns';
 
 function Report() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [dateState, setDateState] = useState(new Date());
-  const changeDate = (e) => {
-    setDateState(e);
-  };
+
+  function validDateRange(start, end) {
+    const errDate = document.getElementById('errDate');
+    if (start > end) {
+      errDate.style.display = 'block';
+      errDate.innerHTML = 'Start date have to small than end date.';
+    } else {
+      errDate.style.display = 'none';
+      errDate.innerHTML = '';
+    }
+  }
 
   function selectStartDate(date) {
     setStartDate(date);
+    const start = parseInt(format(date, 'yyyy/MM/dd').replaceAll('/', ''));
+    const end = parseInt(format(endDate, 'yyyy/MM/dd').replaceAll('/', ''));
+    validDateRange(start, end);
   }
+
   function selectEndDate(date) {
     setEndDate(date);
+    const start = parseInt(format(startDate, 'yyyy/MM/dd').replaceAll('/', ''));
+    const end = parseInt(format(date, 'yyyy/MM/dd').replaceAll('/', ''));
+    validDateRange(start, end);
+  }
+
+  function handleSearch(e) {
+    e.preventDefault();
+    const formSearch = document.getElementById('formSearch');
+    console.log(formSearch.length);
+    for (let i = 0; i < formSearch.length - 1; i++) {
+      console.log(formSearch[i].value);
+    }
   }
 
   return (
@@ -29,11 +54,11 @@ function Report() {
             <Card>
               <CardHeader>
                 <div className="report">
-                  <div className="report__info">
+                  <form id="formSearch" className="report__info">
                     <p className="report__group">Aggregation period:</p>
                     <div className="report__group">
                       <select name="" id="">
-                        <option value="">Aggregation period</option>
+                        <option value="qqq">Aggregation period</option>
                         <option value="">The day before</option>
                         <option value="">Last 7 days</option>
                         <option value="">last 30 days</option>
@@ -41,6 +66,7 @@ function Report() {
                     </div>
                     <div className="report__group report-date">
                       <DatePicker
+                        id="startDate"
                         selected={startDate}
                         onChange={(date) => selectStartDate(date)}
                         dateFormat="yyyy/MM/dd"
@@ -48,6 +74,7 @@ function Report() {
                     </div>
                     <div className="report__group report-date">
                       <DatePicker
+                        id="endDate"
                         selected={endDate}
                         onChange={(date) => selectEndDate(date)}
                         dateFormat="yyyy/MM/dd"
@@ -56,7 +83,7 @@ function Report() {
                     <p className="report__group">device</p>
                     <div className="report__group">
                       <select name="" id="">
-                        <option value="">All</option>
+                        <option value="qqqwe">All</option>
                         <option value="">computer</option>
                         <option value="">Tablet</option>
                         <option value="">Smart phone</option>
@@ -65,17 +92,18 @@ function Report() {
                     <p className="report__group">scenario</p>
                     <div className="report__group">
                       <select name="" id="">
-                        <option value="">BOB scenario</option>
+                        <option value="dddd">BOB scenario</option>
                         <option value="">Test1</option>
                         <option value="">Test2</option>
                       </select>
                     </div>
                     <div className="report__group">
-                      <button className="btn btn-primary">
+                      <button className="btn btn-primary" onClick={(e) => handleSearch(e)}>
                         <i class="fa fa-search" aria-hidden="true"></i>
                       </button>
                     </div>
-                  </div>
+                    <span id="errDate" className="err-date"></span>
+                  </form>
                   <div className="report__download">
                     <button className="btn btn-primary">Input contents download</button>
                     <button className="btn btn-primary">download</button>
@@ -86,9 +114,8 @@ function Report() {
                 <div className="report__body">
                   <div className="report__item">
                     <div className="report__item-head">
-                      CONVERSION RATE (CVR) / CLICK-THROUGH RATE (CTR){'  '}
+                      CONVERSION RATE (CVR) / CLICK-THROUGH RATE (CTR
                       <a href="">
-                        {' '}
                         <i class="far fa-question-circle"></i>
                       </a>
                     </div>
@@ -101,31 +128,30 @@ function Report() {
 
                   <div className="report__item report__item-2">
                     <div className="report__item-head report__item-2-head">
-                      NUMBER OF CONVERSIONS / NUMBER OF BOT STARTS {'  '}
+                      NUMBER OF CONVERSIONS / NUMBER OF BOT STARTS
                       <a href="">
-                        {' '}
                         <i class="far fa-question-circle"></i>
                       </a>
                     </div>
                     <div className="report__item-head report__item-2-head">
-                      CHANGE IN MONTHLY CONVERSIONS {'  '}
+                      CHANGE IN MONTHLY CONVERSIONS
                       <a href="">
-                        {' '}
                         <i class="far fa-question-circle"></i>
                       </a>
                       <Calendar
                         className="report__item-2-head-calender"
                         value={dateState}
-                        onChange={changeDate}
+                        onChange={(e) => {
+                          setDateState(e);
+                        }}
                       />
                     </div>
                   </div>
 
                   <div className="report__item">
                     <div className="report__item-head">
-                      CONTENT {'  '}
+                      CONTENT
                       <a href="">
-                        {' '}
                         <i class="far fa-question-circle"></i>
                       </a>
                       <div className="report__item-btn">
@@ -152,9 +178,8 @@ function Report() {
 
                   <div className="report__item report__item-2">
                     <div className="report__item-head report__item-2-head">
-                      AREA {'  '}
+                      AREA
                       <a href="">
-                        {' '}
                         <i class="far fa-question-circle"></i>
                       </a>
                       <div className="report__item-btn">
@@ -163,9 +188,8 @@ function Report() {
                       </div>
                     </div>
                     <div className="report__item-head report__item-2-head">
-                      DEVICE {'  '}
+                      DEVICE
                       <a href="">
-                        {' '}
                         <i class="far fa-question-circle"></i>
                       </a>
                     </div>
