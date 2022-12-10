1190dcebbcded8654252b242f49f31432b211652
nghia.hoang <nghia.hoang@rikai.technology>
2022-12-10 22:52:49 +0700

    Translate screen Installation tag & demo

--- Instagram_Chatbot/src/views/BotElement/BotSetting/InstallationTag.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/InstallationTag.jsx
@@ -31,11 +31,11 @@ function InstallationTag() {
           <Col md="12">
             <Card>
               <CardHeader>
-                <h4 style={{ color: '#767676' }}>Installation guide</h4>
+                <h4 style={{ color: '#767676' }}>設置ガイド</h4>
               </CardHeader>
               <CardBody>
-                <span style={{ color: '#767676' }}>Embed a bot on your site</span> <br />
-                <span style={{ color: '#767676', fontWeight: "400" }}>Paste the following code inside the &lt;body&gt; tag of your website to display the webchat in the bottom right corner of the page.</span>
+                <span style={{ color: '#767676' }}>サイトにボットを埋め込む</span> <br />
+                <span style={{ color: '#767676', fontWeight: "400" }}>ページの右下にウェブチャットを表示するためにウェブサイトの &lt;body&gt; タグ内に以下のコードを貼り付けてください。</span>
                 <div style={{ width: "90%", border: '1px solid grey', color: '#767676', padding: '5px', borderRadius: '5px' }}>
                   &lt;script&gt; sessionStorage.setItem("bot_id", "{botId}"); sessionStorage.setItem("scenario_id", "{scenarioIdSelected}");&lt;/script&gt; <br />
                   &lt;script src="https://ec-chatbot-test1.com/sdk.js" defer&gt;&lt;/script&gt;
@@ -45,12 +45,12 @@ function InstallationTag() {
                   <span style={{ color: '#767676' }}>Demo URL</span>
                 </div>
                 <div style={{ color: "#767676" }}>
-                  Try chat on the demo site below. It is not reflected in the conversation log and report results. <br />
+                <p>以下のデモサイトでチャットをお試しください。会話ログとレポート結果に反映されません。</p>
                   <Link to={urlDemo}>
                     <button style={{ padding: "5px 35px", border: 'none', borderRadius: "10px", backgroundColor: "#6bd198", color: '#FFFFFF' }}>デモ</button>
                   </Link>
                   <br />
-                  * By adding /bot ID to the end of the URL, you can run a specific scenario that is already selected in list scenario.
+                  {/* * By adding /bot ID to the end of the URL, you can run a specific scenario that is already selected in list scenario. */}
                   <br /> <br />
                 </div>
               </CardBody>
