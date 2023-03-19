4d892192d2a0e43e5be47eb3e325109702d81e69
imindesign96 <phanngocvu89@gmail.com>
2023-03-19 15:02:53 +0900

    Change url

--- Instagram_Chatbot/src/variables/constants.js
+++ Instagram_Chatbot/src/variables/constants.js
@@ -1,8 +1,44 @@
 export const ADD_TOKEN="ADD_TOKEN";
 
-export const S3_UPLOAD_URL="https://ec-chatbot.s3.ap-northeast-1.amazonaws.com/";
-// export const SHORTEN_URL = "https://localhost:3000/s/";
-export const SHORTEN_URL = "https://ec-chatbot1.com/s/";
-export const TEST_URL = "https://ec-chatbot-test1.com"
-export const EC_CHATBOT_URL = "https://ec-chatbot-test.com"
+const getEnvironment = () => {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
 
+    return params.env || "production";
+}
+
+const getDebugFlag = () =>  {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+
+    return params.debug || true;
+}
+
+const log = (message) => {
+    let debugFlag = getDebugFlag();
+
+    if (debugFlag) {
+        console.log(message);
+    }
+}
+
+const getEcChatBotApiServerBaseUrl = () => {
+    const environment = getEnvironment();
+    log("Environment: " + environment);
+
+    switch (environment) {
+        case "staging":
+        case "test": 
+            return "http://ec-chatbot1.com";
+        case "production":
+            return "http://ec-chatbot.com";
+        default: 
+            return "http://localhost:3000";
+    }
+}
+
+export const S3_UPLOAD_URL= "https://ec-chatbot.s3.ap-northeast-1.amazonaws.com/";
+export const SHORTEN_URL = `${getEcChatBotApiServerBaseUrl()}/s/`;
+export const EC_CHATBOT_URL = `${getEcChatBotApiServerBaseUrl()}`;
