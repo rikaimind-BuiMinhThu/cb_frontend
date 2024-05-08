edc858395dfe6f1f8760131a902404978a74d82b
minhdt <minhdt@gmail.com>
2024-05-08 10:38:24 +0700

    update productVariant

--- Instagram_Chatbot/src/layouts/Admin.js
+++ Instagram_Chatbot/src/layouts/Admin.js
@@ -35,10 +35,10 @@ function Dashboard(props) {
     const pathname = location.pathname;
     // const client = JSON.parse(sessionStorage.getItem('client'));
     const client = JSON.parse(localStorage.getItem('client'));
-    const isInstagram = client?.is_instagram;
-    const isWeb = client?.is_web;
-    // const isInstagram = true;
-    // const isWeb = true;
+    // const isInstagram = client?.is_instagram;
+    // const isWeb = client?.is_web;
+    const isInstagram = true;
+    const isWeb = true;
     const instagramRolesUrl = ['/admin/chatbot', '/admin/keyword', '/admin/release', '/admin/data', '/admin/data-analyst', '/admin/list-user', '/admin/attracted-customer', '/admin/crm'];
     const webRolesUrl = ['/admin/bot', '/admin/account-information', '/admin/basic-setting', '/admin/reply-mail-management'];
     if ((!isInstagram && instagramRolesUrl.includes(pathname)) || (!isWeb && webRolesUrl.includes(pathname))) {
