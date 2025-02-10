4f3b2845f51e28094d20a37247bd7a997f1ea931
Hoang Long <long.hoang@rikai.technology>
2025-02-10 14:14:47 +0700

    update condition

--- Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewClone.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewClone.jsx
@@ -652,7 +652,9 @@ function Preview() {
               messageArr = [...res.data.data?.conversation?.messages.filter(x => !x.hidden)];
               if(isLoggedIn)
               {
-                messageArr = [...res.data.data?.conversation?.messages.filter(x => !x.hidden || x.not_display_when_logged_in==false || x.not_display_when_logged_in==undefined)]
+                messageArr = messageArr.filter(
+                  x => x.not_display_when_logged_in !== true
+                );
               }
             }
             let urlThanks = res.data.data?.conversation?.urlThanksPage || "";
