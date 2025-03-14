7a3c549f40f8768d866526157cf7c93958e46393
Hau Nguyen <hau.nguyen@rikai.techonology>
2025-03-14 16:22:36 +0700

    fix render

--- Instagram_Chatbot/src/views/BotElement/BotSetting/ScenarioSetting/Scenario.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/ScenarioSetting/Scenario.jsx
@@ -13494,7 +13494,7 @@ const Scenario = () => {
           <span style={{ fontSize: '16px' }}>{messageNoti}</span>
         </div>
       </ModalNoti>
-      {isOpenModalCustomCss && renderModalCustomCssForm(isOpenModalCustomCss)}
+      {renderModalCustomCssForm(isOpenModalCustomCss)}
       <ModalShort open={isOpenAddVariable} onClose={() => setIsOpenAddVariable(false)}>
         <div className="sl-popup-create-scenario-wrapper">
           <h4>変数追加</h4>
