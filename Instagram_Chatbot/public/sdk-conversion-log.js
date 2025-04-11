399ebe56d172d5e0719af44dcd816ffee97c8dfe
ThuBui <buiminhthuk55@gmail.com>
2025-04-11 23:26:59 +0700

    Fix patchToChatBotServer

--- Instagram_Chatbot/public/sdk-conversion-log.js
+++ Instagram_Chatbot/public/sdk-conversion-log.js
@@ -1,6 +1,7 @@
 (() => {
+  const scenarioId = getUrlParameter(EC_CHATBOT_CONVERSION_PARAMS.SCENARIO_ID) || localStorage.getItem(EC_CHATBOT_STORAGE_KEYS.SCENARIO_ID);
   const params = {
-    scenario_id: getUrlParameter(EC_CHATBOT_CONVERSION_PARAMS.SCENARIO_ID) || localStorage.getItem(EC_CHATBOT_STORAGE_KEYS.SCENARIO_ID),
+    scenario_id: scenarioId,
     bot_type: getUrlParameter(EC_CHATBOT_CONVERSION_PARAMS.BOT_TYPE) || localStorage.getItem(EC_CHATBOT_STORAGE_KEYS.BOT_TYPE),
     user_input_id: getUrlParameter(EC_CHATBOT_CONVERSION_PARAMS.USER_INPUT_ID) || localStorage.getItem(EC_CHATBOT_STORAGE_KEYS.USER_INPUT_ID)
   };
@@ -34,5 +35,5 @@
     scenario_data: `${device}_conversion`,
   };
 
-  sendCountRequest(conversion).then(res => console.log(res));
+  sendCountRequest(scenarioId, conversion).then(res => console.log(res));
 })();
