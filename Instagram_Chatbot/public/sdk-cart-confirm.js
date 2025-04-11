190ab644ecdd09ef8a8f91fbd73fb02d2079b6a2
ThuBui <buiminhthuk55@gmail.com>
2025-04-11 08:57:29 +0700

    Refactor

--- Instagram_Chatbot/public/sdk-cart-confirm.js
+++ Instagram_Chatbot/public/sdk-cart-confirm.js
@@ -1,32 +1,8 @@
-// Script for cart confirm page
 (() => {
-  // Define constant keys
-  const STORAGE_KEYS = {
-    SCENARIO_ID: 'ecChatbotScenarioId',
-    BOT_TYPE: 'ecChatbotBotType',
-    USER_INPUT_ID: 'ecChatbotUserInputId'
-  };
-
-  // Define URL parameter names
-  const URL_PARAMS = {
-    SCENARIO_ID: 'scenario_id',
-    BOT_TYPE: 'bot_type',
-    USER_INPUT_ID: 'user_input_id'
-  };
-
-  // Hàm lấy parameter từ URL
-  const getUrlParameter = name => {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    const regex = new RegExp(`[\\?&]${name}=([^&#]*)`);
-    const results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-  };
-
-  // Mapping giữa URL parameters và localStorage keys
   const paramsMapping = {
-    [URL_PARAMS.SCENARIO_ID]: STORAGE_KEYS.SCENARIO_ID,
-    [URL_PARAMS.BOT_TYPE]: STORAGE_KEYS.BOT_TYPE,
-    [URL_PARAMS.USER_INPUT_ID]: STORAGE_KEYS.USER_INPUT_ID
+    [EC_CHATBOT_CONVERSION_PARAMS.SCENARIO_ID]: EC_CHATBOT_STORAGE_KEYS.SCENARIO_ID,
+    [EC_CHATBOT_CONVERSION_PARAMS.BOT_TYPE]: EC_CHATBOT_STORAGE_KEYS.BOT_TYPE,
+    [EC_CHATBOT_CONVERSION_PARAMS.USER_INPUT_ID]: EC_CHATBOT_STORAGE_KEYS.USER_INPUT_ID
   };
 
   // Lưu parameters vào localStorage
