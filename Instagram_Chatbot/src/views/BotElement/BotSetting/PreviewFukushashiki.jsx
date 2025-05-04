1173ee4b7ce6c4244fd42af6da7e308a256cd03c
ThuBui <buiminhthuk55@gmail.com>
2025-05-04 18:35:52 +0700

    Fix bug

--- Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewFukushashiki.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewFukushashiki.jsx
@@ -142,11 +142,18 @@ const PreviewFukushashikiReducer = (state, action) => {
     case PREVIEW_ACTIONS.UPDATE_RENDER_MESSAGES:
       return { ...state, renderMessagesList: action.payload };
     case PREVIEW_ACTIONS.UPDATE_SUBMIT_ERROR_MESSAGE:
+      const messagesList = _.cloneDeep(state.messagesList);
+
       if (action.payload === NO_ERROR) {
-        return { ...state, submitErrorMessage: action.payload };
+        for (let i = state.currentMsgIndex + 1; i > 0; i--) {
+          const message = messagesList[i];
+          if (message.message_content?.find(content => content.type === 'getting_error_notification')) {
+            message.hidden = true;
+          }
+        }
+        return { ...state, submitErrorMessage: action.payload, messagesList: messagesList };
       }
 
-      const messagesList = _.cloneDeep(state.messagesList);
       messagesList.forEach(message => {
         if (message.hidden === false) {
           message.hidden = action.payload && message.not_display_when_have_error;
