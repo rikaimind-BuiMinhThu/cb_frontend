aab046fbc9135cdfc77ffe378b2d5c0fba6a85b1
ThuBui <buiminhthuk55@gmail.com>
2025-09-15 00:35:31 +0900

    Fix display of logo chatbot in header when open

--- Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewFukushashiki.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewFukushashiki.jsx
@@ -4524,7 +4524,7 @@ const PreviewFukushashiki = () => {
           className="sp-header"
         >
           <div className="sp-header-left" onClick={handleCloseChatbotWhenUseWithDrawal}>
-            <div className="sp-header-left-avatar sp-avatar-bt">
+            <div className="sp-body-bot-side-avatar sp-avatar">
               <img src={`${EC_CHATBOT_URL}${getBotHeaderIcon()}`} alt="bot-header-icon"/>
             </div>
             <div className="sp-header-left-label">
@@ -4653,7 +4653,7 @@ const PreviewFukushashiki = () => {
         }}
       >
         <div className="sp-header-left-bt" onClick={() => onOpenPreview(!state.isOpen)}>
-          <div className="sp-header-left-avatar sp-avatar-bt">
+          <div className="sp-body-bot-side-avatar sp-avatar">
             <img src={`${EC_CHATBOT_URL}${getBotHeaderIcon()}`} alt="bot-header-icon" />
           </div>
         </div>
