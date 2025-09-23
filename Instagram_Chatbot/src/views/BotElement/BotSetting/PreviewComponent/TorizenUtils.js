1f5154972f62bc5ef8f9db2a84c7b415f96e7297
ThuBui <buiminhthuk55@gmail.com>
2025-09-24 05:20:50 +0900

    Refactor: Add comment out for test torizen in local

--- Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewComponent/TorizenUtils.js
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewComponent/TorizenUtils.js
@@ -87,6 +87,7 @@ export const mapAmazonPayDataToMessagesList = (amazonPayData, messagesList, pref
 // TODO: Cần refactor đoạn này sao cho có thể sử dụng được setting RENDER_MODES trên trang quản lý
 export const isTorizenLP = (url) => {
   const torizenDomains = [
+    // Comment out if you want to test torizen in localhost
     // "localhost:8080",
     "commerceforce.co.jp",
     "hana.inuneko-sukoyaka.jp",
