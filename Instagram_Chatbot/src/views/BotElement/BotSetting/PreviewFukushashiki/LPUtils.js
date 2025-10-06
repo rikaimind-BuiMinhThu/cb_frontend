1bacefb216f955d3207a17938827de3e971ec52b
RIKAI-MIND Phan Duc Duy <anh.nguyen2@rikai.technology>
2025-10-06 14:29:48 +0700

    Revert code

--- Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewFukushashiki/LPUtils.js
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewFukushashiki/LPUtils.js
@@ -42,10 +42,10 @@ const fukushashikiSavedStateToLp = (savedState, params, state) => {
       // Except some data when fukushashiki torizen san
       if (params.get('is_using_amazon_pay') && isTorizenLpAmazonData(message)) return;
       
-      const fukuData = convertToFukushashikiObject({ message });
+      const fukuData = convertToFukushashikiObject({message: message});
       fukuDataList.push(...fukuData);
     });
-    
+
     fukushashikiToLP(fukuDataList, savedState);
     resolve();
   });
