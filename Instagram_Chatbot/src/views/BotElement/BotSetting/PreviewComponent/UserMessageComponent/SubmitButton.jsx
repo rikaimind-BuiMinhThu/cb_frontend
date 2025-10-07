47a6f03bb83415ce5f19a3e181bba6901107d639
Duy Ngu <duy.phanduc2005@gmail.com>
2025-10-08 03:43:56 +0700

    Fix: hot-fix production

--- Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewComponent/UserMessageComponent/SubmitButton.jsx
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewComponent/UserMessageComponent/SubmitButton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "assets/css/bot/preview-chat-bot.css";
 
 export default function SubmitButton({ content, submitErrorMessage = "", onClickNext, isProcessing = false }) {
