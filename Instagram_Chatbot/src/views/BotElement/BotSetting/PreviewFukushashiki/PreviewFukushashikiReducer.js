14590c064ad64593bc9033c21c4aeb8523375f2b
hanguyenba <habe2k4@gmail.com>
2025-10-11 11:01:22 +0700

    Fix passedUserMsgCount logic in PreviewFukushashikiReducer

--- Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewFukushashiki/PreviewFukushashikiReducer.js
+++ Instagram_Chatbot/src/views/BotElement/BotSetting/PreviewFukushashiki/PreviewFukushashikiReducer.js
@@ -199,10 +199,10 @@ const PreviewFukushashikiReducer = (state, action) => {
 
       newState.renderMessagesList = newState.messagesList.slice(0, newState.currentMsgIndex + 1);
       newState.isUpdateClicked = isUpdateClicked;
-      if (!isUpdateClicked) {
+      if (renderMode === 'next') {
         newState.passedUserMsgCount = state.passedUserMsgCount + 1;
       } else {
-        newState.passedUserMsgCount = state.passedcUserMsgCount;
+        newState.passedUserMsgCount = newState.renderMessagesList.filter(m => isUserMessage(m)).length;
       }
       return { ...state, ...newState };
     case PREVIEW_ACTIONS.UPDATE_PREFECTURES_LIST:
