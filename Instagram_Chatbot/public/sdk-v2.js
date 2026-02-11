60826a8e069a7154fa4bb3a577f40bc2c57cfad2
hanh.tran <hanh.tran@rikai.technology>
2026-02-11 16:25:46 +0700

    Update RoseMay domain handling in SDK
    - Commented out the old RoseMay domain "rosemay.jp" and added "rosemay.net" to the list of domains for URL checks.

--- Instagram_Chatbot/public/sdk-v2.js
+++ Instagram_Chatbot/public/sdk-v2.js
@@ -455,7 +455,8 @@ const isBlissLp = (url) => {
 const isRoseMayLp = (url) => {
   const domains = [
     // TODO: Update RoseMay domains nếu cần test ở môi trường khác
-    "rosemay.jp",
+    // "rosemay.jp",
+    "rosemay.net",
   ];
 
   return domains.some(domain => url.includes(domain));
